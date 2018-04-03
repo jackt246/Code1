6ce2528bff234f562a20026ac52db296e2a8b5ce
jackt246 <37897473+jackt246@users.noreply.github.com>
2018-04-03 09:44:01 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,8 @@
 import os
 import numpy as np
-import readtxt
-from readtxt import *
 import math
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class Three_Dimensional_Object:
     """
@@ -19,6 +18,7 @@ class Three_Dimensional_Object:
         self.point1 = np.array([x1,y1,z1])
         self.point2 = np.array([x2,y2,z2])
         self.Vector = self.point1 - self.point2
+        self.linecenter = np.add(self.point1, self.point2) / 2
 
     def Info(self):
         return 'point 1 = {}, Point 2 = {}, Vector = {}'.format(self.point1, self.point2, self.Vector)
@@ -43,6 +43,21 @@ class Three_Dimensional_Object:
         linelength = math.sqrt(linedistancesquared)
         return linelength
 
+    def Translate(self, translationarray):
+        """
+        This function will translate the linecenter value and provide a new center point to draw from
+        when drawing the line back from a vector.
+        """
+        newlinecenterx = self.linecenter[0] + translationarray[0]
+        newlinecentery = self.linecenter[1] + translationarray[1]
+        newlinecenterz = self.linecenter[2] + translationarray[2]
+
+        newcenter = np.array((newlinecenterx, newlinecentery, newlinecenterz))
+
+        return newcenter
+
+
+
 
 def RotationMatrix(axis, rotang):
     """
@@ -59,7 +74,7 @@ def RotationMatrix(axis, rotang):
                      [2*(bc+ad), a2+c2-b2-d2, 2*(cd-ab)],
                      [2*(bd-ac), 2*(cd+ab), a2+d2-b2-c2]])
 
-def AppleRotationMatrix(vector, rotationmatrix):
+def ApplyRotationMatrix(vector, rotationmatrix):
     """
     This function take the output from the RotationMatrix function and uses that to apply the rotation to an input vector
     """
@@ -85,11 +100,68 @@ def CalculateAngleBetweenVector(vector, vector2):
 
     return angleindeg
 
-#def RedrawLine(vector, length, origin):
+def RedrawLine(vector, origin):
+    """
+    This function is designed to take a vector that you have rotated and use to to redraw the original line at the rotated angle
+    vector: the array that describes the new direction
+    length: a single interger that describes the lines length, should be available through the Three_Dimensional_Object.linelength() method
+    Origin: The array hat describes the origin, should be available through Three_Dimensional_Object.linecenter
+    """
+    o = origin
+    v = vector
+
+    p1 = o + v/2
+    p2 = o - v/2
+
+    return p1, p2
+
+def DrawLineGraph(point1, point2):
+    """
+    This function will a line that represents the HA ectodomain in 3D space. It will be useful for ensuring rotations are working correctly.
+    """
+    x = [point1[0], point2[0]]
+    y = [point1[1], point2[1]]
+    z = [point1[2], point2[2]]
 
 
 
-#First we create are line that will be our HA ectodomain
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(x, y, z)
+    ax.set_zlim(0, 100)
+    ax.set_xlim(0, 100)
+    ax.set_ylim(0, 100)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+
+def Draw2LineGraph(point1, point2, point3, point4):
+    """
+    This function will a line that represents the HA ectodomain in 3D space. It will be useful for ensuring rotations are working correctly.
+    """
+    x = [point1[0], point2[0]]
+    y = [point1[1], point2[1]]
+    z = [point1[2], point2[2]]
+
+    x1 = [point3[0], point4[0]]
+    y1 = [point3[1], point4[1]]
+    z1 = [point3[2], point4[2]]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(x, y, z)
+    ax.plot(x1, y1, z1)
+    ax.set_zlim(0, 100)
+    ax.set_xlim(0, 100)
+    ax.set_ylim(0, 100)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+
+
+# First we create are line that will be our HA ectodomain
 
 Reference_Cylinder = Three_Dimensional_Object(50,50,68,50,50,25)
 
@@ -105,14 +177,28 @@ print('Length of membrane = {}'.format(Reference_Membrane.linelength()))
 
 # Now we carry out a rotation and print out the angle between the first line and the rotated line.
 
-RotationM = RotationMatrix((0,0,1), 18)
+RotationM = RotationMatrix((0,0,1), 0)
 
-RotatedRot = AppleRotationMatrix(Reference_Cylinder.Vector, RotationM)
+RotatedRot = ApplyRotationMatrix(Reference_Cylinder.Vector, RotationM)
 
-RotationM = RotationMatrix((1,0,0), 163)
+RotationM = RotationMatrix((1,0,0), 180)
 
-RotatedTilt = AppleRotationMatrix(RotatedRot, RotationM)
+RotatedTilt = ApplyRotationMatrix(RotatedRot, RotationM)
 
-print RotatedTilt
+print('Rotated coords = {}'.format(RotatedTilt))
 
+print ('the center point is {}'.format(Reference_Cylinder.linecenter))
 
+p1,p2 = RedrawLine(RotatedTilt, Reference_Cylinder.linecenter)
+
+Draw2LineGraph(p1, p2, Reference_Cylinder.point1, Reference_Cylinder.point2)
+
+TranslateBy = np.array((10,10,0))
+
+translatedcenter = Reference_Cylinder.Translate(TranslateBy)
+
+transp1, transp2 = RedrawLine(RotatedTilt, translatedcenter)
+
+Draw2LineGraph(p1,p2,transp1,transp2)
+
+plt.show()
